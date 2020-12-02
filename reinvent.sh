0c74c52125328640ae99334d232833b293dcbdc9
Jeff Nelson <jeff.nelson@nelnet.net>
2020-12-02 16:10:37 -0600

    Removing Quiet

--- reinvent.sh
+++ reinvent.sh
@@ -30,7 +30,7 @@ streamlink -o ${filename}.ts ${m3u8url} best --hls-live-restart
 
 ## Convert stream to MP4
 echo "${YELLOW}Converting Stream to MP4...${NOCOLOR}"
-ffmpeg -i ${filename}.ts -c copy ${filename}.mp4 quiet
+ffmpeg -i ${filename}.ts -c copy ${filename}.mp4
 
 ## Delete original stream file
 rm ${filename}.ts
